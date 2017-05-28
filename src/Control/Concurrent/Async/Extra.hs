241dc561b3fa95f83111e0b27e65a18c75a1a2d2
Alexander Thiemann <mail@athiemann.net>
2017-05-28 02:29:58 +0200

    switch to split package

--- src/Control/Concurrent/Async/Extra.hs
+++ src/Control/Concurrent/Async/Extra.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Control.Concurrent.Async.Extra
     ( -- * concurrent mapping
@@ -17,11 +16,9 @@ import Control.Concurrent.Async
 import Control.DeepSeq
 import Control.Exception
 import Control.Monad
-import Data.List
-import Data.Sequence (Seq)
+import Data.List.Split (chunksOf)
 import qualified Control.Concurrent.QSem as S
 import qualified Data.Foldable as F
-import qualified Data.Sequence as Seq
 
 -- | Span a green thread for each task, but only execute N tasks
 -- concurrently. Ignore the result
@@ -49,9 +46,9 @@ mapConcurrentlyBatched_ batchSize =
 -- and merge results using provided merge function
 mapConcurrentlyBatched ::
     (NFData b, Foldable t)
-    => Int -> (Seq (Seq b) -> IO r) -> (a -> IO b) -> t a -> IO r
+    => Int -> ([[b]] -> IO r) -> (a -> IO b) -> t a -> IO r
 mapConcurrentlyBatched batchSize merge action items =
-    do let chunks = chunkList batchSize $ F.toList items
+    do let chunks = chunksOf batchSize $ F.toList items
        r <- mapConcurrently (\x -> force <$> mapM action x) chunks
        merge r
 
@@ -65,27 +62,13 @@ mapConcurrentlyChunks_ chunkCount =
 -- each chunk in a dedicated green thread. Then merge results using provided merge function
 mapConcurrentlyChunks ::
     (NFData b, Foldable t)
-    => Int -> (Seq (Seq b) -> IO r) -> (a -> IO b) -> t a -> IO r
+    => Int -> ([[b]] -> IO r) -> (a -> IO b) -> t a -> IO r
 mapConcurrentlyChunks chunkCount merge action items =
     do let listSize = F.length items
            batchSize :: Double
            batchSize = fromIntegral listSize / fromIntegral chunkCount
        mapConcurrentlyBatched (ceiling batchSize) merge action items
 
--- | Chunk a list into chunks of N elements at maximum
-chunkList :: forall a. Int -> [a] -> Seq (Seq a)
-chunkList chunkSize =
-    go 0 Seq.empty
-    where
-      go :: Int -> Seq a -> [a] -> Seq (Seq a)
-      go !size !chunk q
-          | size == chunkSize =
-                Seq.singleton chunk Seq.>< go 0 Seq.empty q
-          | otherwise =
-                case q of
-                  [] -> Seq.singleton chunk
-                  (x : xs) -> go (size + 1) (chunk Seq.|> x) xs
-
--- | Merge all chunks by combining to one list
-mergeConcatAll :: Seq (Seq a) -> [a]
-mergeConcatAll = F.toList . foldl' (Seq.><) Seq.empty . F.toList
+-- | Merge all chunks by combining to one list. (Equiv to 'join')
+mergeConcatAll :: [[a]] -> [a]
+mergeConcatAll = join
